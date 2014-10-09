1e7dc328c8fff812a412e90abfd3a0c8f9c2f924
巴思 <basi.dwz@taobao.com>
2014-10-10 00:02:55 +0800

    fix config

---
+++ config/index.js
@@ -0,0 +1,41 @@
+/*!
+ * MiniNote - config/index.js
+ */
+
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var path = require('path');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var copy = require('copy-to');
+
+var version = require('../package.json').version;
+
+var root = path.dirname(__dirname);
+
+var config = {
+  version: version,
+  port: 8484,
+  enableCluster: false,
+  debug: true,
+  logdir: path.join(root, '.tmp', 'logs'),
+  viewCache: false,
+
+  ots: {
+    accessID: '',
+    accessKey: ''
+  }
+};
+
+var customConfig = path.join(root, 'config/config.js');
+if (fs.existsSync(customConfig)) {
+  copy(require(customConfig)).override(config);
+}
+
+mkdirp.sync(config.logdir);
+
+module.exports = config;
